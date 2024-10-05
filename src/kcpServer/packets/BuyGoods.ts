0b477cdd44b6ab81f345c51fbbb8fb6cebf40dd1
Syca <64587684+Sycamore0@users.noreply.github.com>
2024-10-05 15:06:58 +0800

    Add simple BuyGoods Support
    bugs

---
+++ src/kcpServer/packets/BuyGoods.ts
@@ -0,0 +1,62 @@
+import Packet, { PacketInterface, PacketContext } from '#/packet'
+import { ShopGoods } from '@/types/proto'
+import { RetcodeEnum } from '@/types/proto/enum'
+import Material from '$/material'
+import Player from '$/player'
+
+export interface BuyGoodsReq {
+  buyCount: number
+  shopType: number
+  goods: ShopGoods
+}
+
+export interface BuyGoodsRsp {
+  retcode: RetcodeEnum
+  buyCount: number
+  shopType: number
+  goods: ShopGoods
+  goodsList?: ShopGoods[]
+}
+
+class BuyGoodsPacket extends Packet implements PacketInterface {
+  constructor() {
+    super('BuyGoods')
+  }
+
+  async request(context: PacketContext, data: BuyGoodsReq): Promise<void> {
+    const { game, player } = context
+    const { buyCount, shopType, goods } = data
+
+    const goodsList = await game.shopManager.exportGoodsList(shopType, player)
+
+    // cost
+    if (goods.scoin !== undefined || 0) {
+      player.addMora(-goods.scoin)
+    }
+    if (goods.hcoin !== undefined || 0) {
+      player.addPrimogem(-goods.hcoin)
+    }
+    if (goods.mcoin !== undefined || 0) {
+      player.addGenesisCrystal(-goods.mcoin)
+    }
+
+    const { itemId: goodItemId, count: goodItemCount } = goods.goodsItem
+    const goodItem = await Material.create(player, goodItemId, goodItemCount * buyCount)
+    player.inventory.add(goodItem)
+
+    await this.response(context, {
+      retcode: RetcodeEnum.RET_SUCC,
+      buyCount: buyCount,
+      shopType: shopType,
+      goods: goods,
+      goodsList
+    })
+  }
+
+  async response(context: PacketContext, data: BuyGoodsRsp): Promise<void> {
+    await super.response(context, data)
+  }
+}
+
+let packet: BuyGoodsPacket
+export default (() => packet = packet || new BuyGoodsPacket())()
